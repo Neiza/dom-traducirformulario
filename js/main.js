69e817e94a0874fb9ee3b91ea11d9af6c109d56c
Neiza Nunez <neizapf@gmail.com>
2017-03-03 19:16:49 -0500

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+function translate(){
+  var tittle = document.getElementById('form-signin-heading');
+  var email = document.getElementById('inputEmail');
+  var password = document.getElementById('inputPassword');
+  var remember = document.getElementsByTagName('span')[0];
+  var button = document.getElementsByClassName('btn')[0];
+
+  tittle.innerHTML = "Por favor inicia seión";
+  email.placeholder = "Correo electrónico "
+  password.placeholder = "Contraseña"
+  remember.innerHTML = "Recordar datos"
+  button.innerHTML = "Iniciar sesión"
+}
+translate();
+
+function imprimir(){
+  var records = document.getElementById("record");
+  records.innerHTML= "El "+ document.getElementById('inputEmail').placeholder + " ingresado es <br>"+document.getElementById('inputEmail').value+ "<br>"+
+                    " la clave ingresada es <br>"+ document.getElementById('inputPassword').value;
+
+}
